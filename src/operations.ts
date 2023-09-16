e32c1a7ed520a869e12fcb4e82fcc042116810a2
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-09-16 12:59:40 -0400

    first pass at issue ECR and OOR

--- src/operations.ts
+++ src/operations.ts
@@ -1,3 +1,14 @@
-export const DONE = "done";
-export const NAME = "name";
-export const RESPONSE = "response";
+const DONE = "done";
+const NAME = "name";
+const RESPONSE = "response";
+import { SignifyClient } from "signify-ts";
+
+export const waitKeriaOp = async (client: SignifyClient, op: any, log: boolean = false) => {
+    const ops = client.operations();
+    while (!op[DONE]) {
+        op = await ops.get(op[NAME]);
+        if (log) { console.log(op) }
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+    return op[RESPONSE];
+}
--- src/qvi/qvi.ts
+++ src/qvi/qvi.ts
@@ -1,51 +1,100 @@
-import {SignifyClient} from "signify-ts";
-import {DONE, NAME, RESPONSE} from "../operations";
+import { SignifyClient } from "signify-ts";
+import { waitKeriaOp } from "../operations";
+import { getQviRegistry } from "../registry";
 
 
-// import LE from "../schema/legal-entity-vLEI-credential.json";
+import LE from "../schema/legal-entity-vLEI-credential.json";
+import ECR from "../schema/legal-entity-engagement-context-role-vLEI-credential.json";
+import OOR from "../schema/legal-entity-engagement-context-role-vLEI-credential.json";
+
+
+import {ECRVLEICredential, LEVLEICredential, LE_VLEI_EDGES, ECR_AUTH_EDGES, ECR_LE_EDGES, OOR_AUTH_EDGES, OORVLEICredential} from '../interfaces'
+
 export class QVI {
-    public readonly SCHEMA_CACHE: { LEGAL_ENTITY: any; };
+    private client: SignifyClient;
     private readonly alias: string;
     private readonly registryName: string;
     private readonly registryNonce: string;
     private registry: any;
+    private readonly SCHEMA_CACHE: { LEGAL_ENTITY: any; ENGAGEMENT_CONTEXT_ROLE: any; OFFICIAL_ORGANIZATIONAL_ROLE: any; };
 
     constructor(alias: string, registryName: string, registryNonce: string) {
         this.alias = alias;
         this.registryName = registryName;
         this.registryNonce = registryNonce;
-        let LE = {};
         this.SCHEMA_CACHE = {
             LEGAL_ENTITY: {
                 LE: LE,
-                RULES: [
-                    // @ts-ignore
-                    JSON.stringify(LE.properties.r.oneOf[1].properties.usageDisclaimer.properties.l.const),
-                    // @ts-ignore
-                    JSON.stringify(LE.properties.r.oneOf[1].properties.issuanceDisclaimer.properties.l.const),
-                ],
+                RULES: {
+                    "d": "EDIai3Wkd-Z_4cezz9nYEcCK3KNH5saLvZoS_84JL6NU",
+                    "usageDisclaimer": {
+                        // @ts-ignore
+                        "l": JSON.stringify(LE.properties.r.oneOf[1].properties.usageDisclaimer.properties.l.const)
+                    },
+                    "issuanceDisclaimer": {
+                        // @ts-ignore
+                        "l": JSON.stringify(LE.properties.r.oneOf[1].properties.issuanceDisclaimer.properties.l.const)
+                    }
+                }
+                ,
                 STRING: JSON.stringify(LE)
-            }
-            // etc1
+            },
+            ENGAGEMENT_CONTEXT_ROLE: {
+                ECR: ECR,
+                RULES:
+                {
+                    "d": "EDIai3Wkd-Z_4cezz9nYEcCK3KNH5saLvZoS_84JL6NU",
+                    "usageDisclaimer": {
+                        // @ts-ignore
+                        "l": JSON.stringify(ECR.properties.r.oneOf[1].properties.usageDisclaimer.properties.l.const)
+                    },
+                    "issuanceDisclaimer": {
+                        // @ts-ignore
+                        "l": JSON.stringify(ECR.properties.r.oneOf[1].properties.issuanceDisclaimer.properties.l.const)
+                    },
+                    "privacyDisclaimer": {
+                        // @ts-ignore
+                        "l": JSON.stringify(ECR.properties.r.oneOf[1].properties.privacyDisclaimer.properties.l.const)
+                    }
+                },
+                STRING: JSON.stringify(ECR)
+            },
+            OFFICIAL_ORGANIZATIONAL_ROLE: {
+                OOR: OOR,
+                RULES: {
+                    "d": "EDIai3Wkd-Z_4cezz9nYEcCK3KNH5saLvZoS_84JL6NU",
+                    "usageDisclaimer": {
+                        // @ts-ignore
+                        "l": JSON.stringify(OOR.properties.r.oneOf[1].properties.usageDisclaimer.properties.l.const)
+                    },
+                    "issuanceDisclaimer": {
+                        // @ts-ignore
+                        "l": JSON.stringify(OOR.properties.r.oneOf[1].properties.issuanceDisclaimer.properties.l.const)
+                    }
+                },
+                STRING: JSON.stringify(OOR)
+            },
         };
     }
 
     public async init(client: SignifyClient) {
-        let op = await client.registries().create(this.alias, this.registryName, this.registryNonce);
-        const ops = client.operations();
-        while (!op[DONE]) {
-            op = await ops.get(op[NAME]);
-            await new Promise((resolve) => setTimeout(resolve, 1000));
+        //check if registry already exists
+        let registry = await getQviRegistry(client, this.registryName);
+        if (registry) {
+            this.registry = registry;
+            return;
         }
-        this.registry = op[RESPONSE];
+        let op = await this.client.registries().create(this.alias, this.registryName, this.registryNonce);
+        registry = await waitKeriaOp(this.client, op);
+        this.registry = registry;
     }
 
     public async issueLegalEntityCredential(
         client: SignifyClient,
         alias: string,
         recipient: string,
-        credentialData: any,
-        source: any | undefined,
+        credentialData: LEVLEICredential,
+        source: LE_VLEI_EDGES, //TODO: I believe the only input required is the aid of the qvi-vlei for the n field and the edge can be generated 
         _private?: boolean
     ) {
         return await client
@@ -61,4 +110,49 @@ export class QVI {
                 _private
             );
     }
+
+    public async issueEngagementContextRoleCredential(
+        client: SignifyClient,
+        alias: string,
+        recipient: string,
+        credentialData:ECRVLEICredential,
+        source: ECR_AUTH_EDGES| ECR_LE_EDGES, //TODO: I believe the only input requiresd should be either the said of the auth credential or the said of the vlei credential 
+        _private?: boolean
+    ) {
+        return await client
+            .credentials()
+            .issue(
+                alias,
+                this.registry,
+                this.SCHEMA_CACHE.ENGAGEMENT_CONTEXT_ROLE.STRING,
+                recipient,
+                credentialData,
+                this.SCHEMA_CACHE.ENGAGEMENT_CONTEXT_ROLE.RULES,
+                source,
+                _private
+            );
+    }
+
+    public async issueOfficialOrganizationalRoleCredential(
+        client: SignifyClient,
+        alias: string,
+        recipient: string,
+        credentialData: OORVLEICredential,
+        source: OOR_AUTH_EDGES, //TODO: I believe the only input required should be the said of the auth credential 
+        _private?: boolean
+    ) {
+        return await client
+            .credentials()
+            .issue(
+                alias,
+                this.registry,
+                this.SCHEMA_CACHE.OFFICIAL_ORGANIZATIONAL_ROLE.STRING,
+                recipient,
+                credentialData,
+                this.SCHEMA_CACHE.OFFICIAL_ORGANIZATIONAL_ROLE.RULES,
+                source,
+                _private
+            );
+    }
+    
 }
